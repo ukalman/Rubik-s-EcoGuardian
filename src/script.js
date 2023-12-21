02fd060422cf64a26788a9c6bcfbd42e6d1a2395
ukalman <umutkalman@hotmail.com>
2023-12-22 02:55:50 +0300

    Jump and Basic Movements Added. A placeholder Texture for the ground is added.

--- src/script.js
+++ src/script.js
@@ -9,8 +9,24 @@ import gsap from "gsap";
 import vShader from "./shaders/vertex.glsl";
 import fShader from "./shaders/fragment.glsl";
 import * as dat from "dat.gui";
+import CannonDebugRenderer from "./cannondebugrenderer";
+
+
 
 const canvas = document.getElementById("game-surface");
+
+// Create the Physics World
+const physicsWorld = new CANNON.World({
+  gravity: new CANNON.Vec3(0, -9.81, 0)
+}); 
+const timeStep = 1 / 60;
+
+// Scene
+const scene = new THREE.Scene();
+//var cannonDebugRenderer = new THREE.CannonDebugRenderer( scene, physicsWorld );
+console.log("scene:");
+console.log(scene);
+// var cannonDebugRenderer = new CannonDebugRenderer(scene,physicsWorld);
 
 // RubiksCube Class
 
@@ -30,34 +46,24 @@ class RubiksCube {
 
       // constants
       this.fadeDuration = 0.2;
-      this.runVelocity = 10;
-      this.walkVelocity = 5;
+      this.runVelocity = 30;
+      this.walkVelocity = 20;
 
-  }
+      // rigidbody
+      const rubiksCubeBody = new CANNON.Body({
+        mass: 3,
+        shape: new CANNON.Box(new CANNON.Vec3(2,2,2)), 
+        position: new CANNON.Vec3(0,0,0)
+      });
+      this.rigidbody = rubiksCubeBody;
+      physicsWorld.addBody(rubiksCubeBody);
 
-  moveForward(distance) {
-    // Move the Rubik's cube forward
-  }
-
-  moveBackward(distance) {
-    // Move the Rubik's cube backward
-  }
-
-  moveLeft(distance) {
-    // Move the Rubik's cube to the left
-  }
-
-  moveRight(distance) {
-    // Move the Rubik's cube to the right
   }
 
   jump(){
 
   }
 
-  rotateCamera(deltaX, deltaY) {
-    // Rotate the camera based on mouse movement
-  }
 
   boost() {
     // Implement logic for boosting the Rubik's cube
@@ -66,12 +72,23 @@ class RubiksCube {
   update(frameTime, keysPressed, delta) {
     // Update the Rubik's cube's state, animation, and progression toward the solution
     const directions = ['w','a','s','d'];
+    const space = [' '];
     const directionPressed = directions.some(key => keysPressed[key] == true);
-    
+    const spacePressed = space.some(key => keysPressed[key] == true);
+
     var play = 'Idle';
     if(directionPressed){
       play = 'MoveForward';
     } 
+
+    if(spacePressed){
+      console.log("yeyya space pressed");
+      play = 'Jump';
+      if(directionPressed){
+        play = 'JumpWhileMove';
+      }
+    }
+
 
     if(this.currentAction != play){
       // console.log("animations map: ");
@@ -89,7 +106,7 @@ class RubiksCube {
 
     this.mixer.update(frameTime);
 
-    if(this.currentAction == 'MoveForward'){
+    if(this.currentAction == 'MoveForward' || this.currentAction == 'JumpWhileMove'){
       // calculate towards camera direction
       // angle between the camera view and the character
       var angleYCameraDirection = Math.atan2(
@@ -97,8 +114,6 @@ class RubiksCube {
         (this.camera.position.z - this.model.position.z)
       );
 
-      console.log("keys pressed:");
-      console.log(keysPressed);
       // diagonal movement angle offset
       var directionOffset = this.directionOffset(keysPressed);
       
@@ -106,7 +121,9 @@ class RubiksCube {
       // make the model rotate towards that direction stepwise
       this.rotateQuaternion.setFromAxisAngle(this.rotateAngle, angleYCameraDirection + directionOffset);
       this.model.quaternion.rotateTowards(this.rotateQuaternion, 0.2);
-        
+      // This part is important, just trying something
+      this.rigidbody.quaternion.copy(this.model.quaternion);
+
       // calculate direction
       // based on the previously calculated angles, we calculated the vector which represents the direction into which the model must move
       this.camera.getWorldDirection(this.moveDirection)
@@ -114,14 +131,31 @@ class RubiksCube {
       this.moveDirection.normalize();
       this.moveDirection.applyAxisAngle(this.rotateAngle,directionOffset);
 
+
       // boost/normal velocity 
       const velocity = this.currentAction == 'Boost' ? this.runVelocity : this.walkVelocity;
 
       // move model & camera
       const moveX = this.moveDirection.x * velocity * frameTime;
       const moveZ = this.moveDirection.z * velocity * frameTime;
-      this.model.position.x += moveX;
-      this.model.position.z += moveZ;
+      // this.model.position.x += moveX;
+      // this.model.position.z += moveZ;
+      this.rigidbody.position.x += moveX;
+      this.rigidbody.position.z += moveZ;
+
+      if(this.currentAction == 'JumpWhileMove'){
+        const moveY = new THREE.Vector3(0.0,1.0,0.0).y * frameTime * velocity;
+        console.log("move x: ");
+        console.log(moveX);
+        console.log("move z: ");
+        console.log(moveZ);
+        console.log("move y: ");
+        console.log(moveY);
+        this.rigidbody.position.y += moveY;
+      }
+
+
+      this.updateModelPosition();
       this.updateCameraTarget(moveX, moveZ);
     }
 
@@ -137,9 +171,12 @@ class RubiksCube {
     this.cameraTarget.y = this.model.position.y + 1;
     this.cameraTarget.z = this.model.position.z;
     this.orbitControl.target = this.cameraTarget;
-
   }
 
+  updateModelPosition(){
+    this.model.position.copy(this.rigidbody.position);
+    this.model.quaternion.copy(this.rigidbody.quaternion);
+  }
 
   directionOffset(keysPressed){
     var directionOffset = 0; // w
@@ -184,8 +221,8 @@ class RubiksCube {
   // Add other methods as needed
 }
 
-function initializeRubiksCube(model, mixer, animationsMap, orbitControl, thirdPersonCam){
-  return new RubiksCube(model, mixer, animationsMap, orbitControl, thirdPersonCam);
+function initializeRubiksCube(model, mixer, animationsMap, orbitControl, thirdPersonCam,rigidbody){
+  return new RubiksCube(model, mixer, animationsMap, orbitControl, thirdPersonCam, rigidbody);
 }
 
 // CONTROL KEYS
@@ -198,6 +235,25 @@ document.addEventListener('keyup',(e)=>{
   keysPressed[e.key.toLowerCase()] = false;
 }, false);
 
+// LoadingManager, put it in the textureloader
+const loadingManager = new THREE.LoadingManager();
+loadingManager.onStart = () => {
+  console.log("Start");
+}
+loadingManager.onLoad = () => {
+  console.log("Loading...");
+}
+loadingManager.onProgress = () => {
+  console.log("Progress");
+}
+
+loadingManager.onError = () => {
+  console.log("Error!");
+}
+
+const textureLoader = new THREE.TextureLoader(loadingManager);
+const colorTexture = textureLoader.load("/texture/groundTexture.jpg");
+console.log(colorTexture);
 
 
 // THREE.JS
@@ -211,8 +267,6 @@ gltfLoader.setDRACOLoader(dracoLoader);
 //GUI
 const gui = new dat.GUI();
 
-//Scene
-const scene = new THREE.Scene();
 
 //Environment Map
 const cubeTextureLoader = new THREE.CubeTextureLoader();
@@ -229,26 +283,31 @@ scene.background = envTexture;
 // Lighting
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.7);
-directionalLight.position.set(0, 4, 2);
+directionalLight.position.set(30, 300, 2);
 directionalLight.castShadow = true;
 // Optimize ShadowMap Size
 directionalLight.shadow.mapSize.width = 1024; // default is 512, value should be divisible by 2
 directionalLight.shadow.mapSize.height = 1024;
-scene.add(ambientLight, directionalLight);
+scene.add(ambientLight);
+scene.add(directionalLight);
+const directionalLightHelper = new THREE.DirectionalLightHelper(
+  directionalLight
+);
+scene.add(directionalLightHelper)
 
+gui.add(directionalLight,"intensity").min(0).max(1).step(0.01).name("Intensity Direct");
+gui.add(directionalLight.position,"x").min(-10).max(10).step(0.01).name("X Dir Direct");
+gui.add(directionalLight.position,"y").min(-10).max(10).step(0.01).name("Y Dir Direct");
+// gui.add(directionalLight.shadow.mapSize,"width").min(512).max(4096).step(512).name("Shadow Map Width");
+// gui.add(directionalLight.shadow.mapSize,"height").min(512).max(4096).step(512).name("Shadow Map Height");
 
-
-// Create the Physics World
-const physicsWorld = new CANNON.World({
-  gravity: new CANNON.Vec3(0, -9.81, 0)
-}); 
-const timeStep = 1 / 60;
 
 // Ground Mesh
 const groundGeo = new THREE.PlaneGeometry(300,300);
-const groundMat = new THREE.MeshBasicMaterial({
+const groundMat = new THREE.MeshStandardMaterial({
   color: 0xcff2f2,
   side: THREE.DoubleSide,
+  map: colorTexture
 });
 const groundMesh = new THREE.Mesh(groundGeo,groundMat);
 groundMesh.receiveShadow = true;
@@ -265,11 +324,12 @@ groundBody.position.set(0,-2,0);
 
 // Box Mesh
 const boxGeo = new THREE.BoxGeometry(2,2,2);
-const boxMat = new THREE.MeshBasicMaterial({
+const boxMat = new THREE.MeshStandardMaterial({
   color: 0x00ff00,
-  wireframe:true
+  //wireframe:true
 });
 const boxMesh = new THREE.Mesh(boxGeo,boxMat);
+boxMesh.castShadow = true;
 scene.add(boxMesh);
 
 // Box Rigid Body
@@ -303,10 +363,12 @@ orbitControls.enableDamping = true;
 const renderer = new THREE.WebGLRenderer({
   canvas,
 });
-renderer.setClearColor("#27282c", 1.0);
+//renderer.setClearColor("#27282c", 1.0);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(aspect.width, aspect.height);
 // tell the renderer to render shadow map
-renderer.shadowMap.enabled = true;
+
 
 
 //Resizing
@@ -329,12 +391,14 @@ window.addEventListener("resize", () => {
 //1) Rubiks Cube
 let animationMixer;
 var rubiksCube = null;
-gltfLoader.load("/assets/rubikscube7.glb", (gltf) => {
+gltfLoader.load("/assets/rubikscube10.glb", (gltf) => {
 
 
   const rubiksCubeModel = gltf.scene;
   rubiksCubeModel.traverse(function (object){
-    if (object.isMesh) object.castShadow = true;
+    if (object.isMesh){ 
+      object.castShadow = true;
+    }
   });
   scene.add(rubiksCubeModel);
   animationMixer = new THREE.AnimationMixer(rubiksCubeModel);
@@ -343,8 +407,10 @@ gltfLoader.load("/assets/rubikscube7.glb", (gltf) => {
   clips.forEach((clip)=>{
     animationsMap.set(clip.name, animationMixer.clipAction(clip))
   });
-
-  const clip = THREE.AnimationClip.findByName(clips, 'Jump');
+  console.log(clips);
+  const clip = THREE.AnimationClip.findByName(clips, 'JumpWhileMove');
+  console.log("clip: ");
+  console.log(clip);
 
   const action = animationMixer.clipAction(clip);
   // console.log(action);
@@ -437,7 +503,8 @@ const animate = () => {
     animationMixer.update(frameTime);
   }
   */
-  
+ // cannon js debug renderer
+  // cannonDebugRenderer.update();
   
   if(rubiksCube){
     rubiksCube.update(frameTime,keysPressed,mixerUpdateDelta);
